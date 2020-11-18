40b21a46d0109c8026206b4489201d0be6d0f380
juanGreiser <rodriguezgreiserjuan@gmail.com>
2020-11-17 21:25:10 -0300

    clases 4-5-6

---
+++ calculadora-class4/calculadora.js
@@ -0,0 +1,10 @@
+let op = require('./operaciones');
+
+console.log(op);
+
+let a = 2;
+let b = 4;
+
+let opereta= op.multiplicacion(a, b);
+
+console.log(opereta)
---
+++ calculadora-class4/operaciones.js
@@ -0,0 +1,17 @@
+let operaciones = {
+    suma: function(a, b){
+        return a + b;
+    },
+    resta: function(a, b){
+        return a - b;
+    },
+    multiplicacion: function(a, b){
+        return a* b;
+    },
+    divicion: function(a, b){
+        return a/b;
+    }
+    
+}
+
+module.exports = operaciones;
